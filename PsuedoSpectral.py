76e9c578fcffa2bfda1d3eb5d636c90e33685771
Dominic Gastaldo <dgastaldo@umassd.edu>
2017-04-07 15:36:12 -0400

    Code Update

--- PsuedoSpectral.py
+++ PsuedoSpectral.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math as m
 import matplotlib.pyplot as plt
+import time
 
 def kroneckerDelta(i,j):
     if i == j:
@@ -8,7 +9,7 @@ def kroneckerDelta(i,j):
     else:
         return 0
 #*******************************************************************************
-    
+        
 def C(j, numberofCollocationPoints):
     return 1 + kroneckerDelta(j,0) + kroneckerDelta(j,numberofCollocationPoints)
     
@@ -17,7 +18,7 @@ def C(j, numberofCollocationPoints):
 def GenerateChebyshevCollocationPoints(numberOfPoints):
     
     a = []
-    
+    a
     c=0.0
     
     while c <= numberOfPoints:
@@ -105,7 +106,7 @@ def ConstructPseudoSpectralDiffMatrix(N):
     
 #*******************************************************************************
 
-def EnforceBoundaryConditionZero(D):
+def EnforceDirichletBoundaryConditionZero(D):
     
     D = D.tolist()
     
@@ -123,7 +124,7 @@ def EnforceBoundaryConditionZero(D):
     
 #*******************************************************************************
     
-def EnforceBoundaryConditionOne(D):
+def EnforceDirichletBoundaryCondition(D):
     
     
     D = D.tolist()
@@ -151,30 +152,6 @@ def EnforceBoundaryConditionOne(D):
 
 #*******************************************************************************
     
-#Need to be numpy arrays
-def waveEquationChebyshevExpansionCoefficients(initCoefficients, numberofIterations):
-    
-    L =len(initCoefficients)
-    D =  ConstructPseudoSpectralDiffMatrix(L)
-    Dxx = np.dot(D,D)
-    
-    temp=0
-    n=2
-    while n < numberofIterations:
-        
-        temp = (h/c)**2 * np.dot(Dxx,u[n-1]) + 2*u[n-1] - u[n-2]
-        # Temp will be a matrix...[[entries]]. Must convert to array...[entries]
-        temp = np.array(temp.tolist()[0])
-        
-        u.append(temp)
-        
-        n = n + 1
-        
-    return u
-
-
-#*******************************************************************************
-
 def chebyshevPolynomial(x,k):
     
     #do time complexity analysis
@@ -218,23 +195,36 @@ def chebyshevPolynomialRecursive(x,k):
 #*******************************************************************************
 
 #Input numpy array
-def PsuedoSpectralExpansionCoefficientsToHeatEquation(initCoefficients, numberofIterations):
+def PsuedoSpectralSolutiontoHeatEquation(initCoefficients, numberofIterations):
     
-    L =len(initCoefficients)
+    L = len(initCoefficients) 
     D =  ConstructPseudoSpectralDiffMatrix(L)
     Dxx = np.dot(D,D)
-    u = [initCoefficients]
+    Dxx = EnforceDirichletBoundaryCondition(Dxx)
+    u = [initCoefficients] 
+   
+    
+    #Enforce Dirichlet BCs to vector
+    u = u[0].tolist()  
+    u.pop(0)
+    u.pop(len(u)-1)
+    u = [np.array(u)]
     
     
-    k=1
+    k=1.0
     t=0.001
     temp=0
     c=0
     while c < numberofIterations:
         
-        temp = -t*k*np.dot(Dxx,u[c]) + u[c]
+        temp = t*k*np.dot(Dxx,u[c]) + u[c]
         # Temp will be a matrix...[[entries]]. Must convert to array...[entries]
         temp = np.array(temp.tolist()[0])
+        
+        #BC
+        temp[0] = 0
+        temp[len(temp)-1] = 0
+            
         
         u.append(temp)
     
@@ -245,68 +235,169 @@ def PsuedoSpectralExpansionCoefficientsToHeatEquation(initCoefficients, numberof
 
 
 #*******************************************************************************
-# y = y' test
 
-def PsuedoSpectralSolutionToHeatEquation(x,initCoefficients, numberofIterations):
+#Input numpy array 
+def PsuedoSpectralSolutionToWaveEquation(initPosition, initMomentum, numberofIterations):
     
     
-    #Check!!!!!!!!
-    
-    a = []
-    c = []
+    L = len(initPosition) 
+    Dx = ConstructPseudoSpectralDiffMatrix(L)
+    Dx = EnforceDirichletBoundaryCondition(Dx)
     u = []
-    
-    
-    a = PsuedoSpectralExpansionCoefficientsToHeatEquation(initCoefficients, numberofIterations)
-    print(a)
-    print("")
-    c = GenerateChebyshevCollocationPoints(len(initCoefficients)) #numberofIterations = number of points?
-    print(str(len(a)) + "<---")
-    
-    t=0
-    while t < numberofIterations:
-        
-       #Loop entered and run correct number of times
+    p=[]
         
         
-        n=0
-        temp=0
-        while n < len(initCoefficients):
-            
-            temp += a[t][n]*chebyshevPolynomial(x,n)
-            n = n + 1
-            
-        u.append(temp)
-        t = t + 1
+    #Enforce Dirichlet BCs to initPosition
+    initPosition = initPosition.tolist() 
+    initPosition.pop(0)
+    initPosition.pop(len(initPosition)-1)
+    initPosition = np.array(initPosition)
+    
+    
+    
+    #Enforce Dirichlet BCs to initMomentum
+    initMomentum = initMomentum.tolist() 
+    initMomentum.pop(0)
+    initMomentum.pop(len(initMomentum)-1)
+    initMomentum = np.array(initMomentum)
+    
+    
+        
+    u.append(initPosition)
+    p.append(initMomentum)
+    
+        
+    
+    t=0.001
+    tempu = 0
+    tempp = 0
+    k=0
+    while k < numberofIterations:
+        
+        tempu = t*(p[k] - np.dot(Dx,u[k])) + u[k]
+        #Tempu will be a matrix...[[entries]]. Must convert to array...[entries]
+        tempu = np.array(tempu.tolist()[0])
+        #BC U
+        tempu[0] = 0
+        tempu[len(tempu)-1] = 0
+        
+    
+    
+        tempp = t*np.dot(Dx,p[k]) + p[k]
+        #Tempp will be a matrix...[[entries]]. Must convert to array...[entries]
+        tempp = np.array(tempp.tolist()[0])
+        #BC U
+        tempp[0] = 0
+        tempp[len(tempp)-1] = 0        
+    
+    
+        u.append(tempu)
+        p.append(tempp)
+    
+    
+        k = k + 1
+        
     
     return u
-
-#*******************************************************************************
-
+    
+#Input numpy array 
+def PsuedoSpectralSolutionToReflectingWaveEquation(initPosition, initMomentum, numberofIterations):
+    
+    
+    L = len(initPosition) 
+    Dx = ConstructPseudoSpectralDiffMatrix(L)
+    u = []
+    p=[]
+    
+    u.append(initPosition)
+    p.append(initMomentum)
         
-def PsuedoSpectralSolutionToWaveEquation():
     
-    #NEED TO FIX!!!
-    
-    #T grows very quickly as n -> inf!
-    #Should x be substituted?
-    
-    a = waveEquationChebyshevExpansionCoefficients()
-    
-    colPoints = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-    
-    U = []
-    for i in a:
-        u=0
-        c=0
-        for j in i:
-            
-            u += j*chebyshevPolynomial(colPoints[c],c)
-            c = c + 1
-            
-        U.append(u)
+    t=0.001
+    tempu = 0
+    tempp = 0
+    k=0
+    while k < numberofIterations:
         
-    return U
+        tempu = t*(p[k] - np.dot(Dx,u[k])) + u[k]
+        # Reflecting BC uxx0 = 0
+        tempu = t* np.dot(Dx,u[k])
+        #Tempu will be a matrix...[[entries]]. Must convert to array...[entries]
+        tempu = np.array(tempu.tolist()[0])
+        # Reflecting boundary conditions ux0 = 0 => uxx0 = 0
+        # Strong boundary imposition: force u0 to be the boundary value you want
+        # In this case, we're enforcing boundary conditions on the left boundary, the left most element u0.
+        
+    
+        tempp = t*np.dot(Dx,p[k]) + p[k]
+        #Tempp will be a matrix...[[entries]]. Must convert to array...[entries]
+        tempp = np.array(tempp.tolist()[0])
+        # Reflecting boundary conditions p0 -> -p0
+        # Strong boundary imposition: force p0 to be the boundary value you want
+        
+    
+    
+        u.append(tempu)
+        p.append(tempp)
+    
+    
+        k = k + 1
+        
+    
+    return u
+    
+def FullsolnHeatEquation():
+    
+    
+    C = GenerateChebyshevCollocationPoints(20)
+    C.sort()
+
+    
+    A = []
+
+    for i in C:
+    
+        A.append(m.exp(-20.0*i**2))
+    
+    A = np.array(A)
+    SSP = PsuedoSpectralSolutiontoHeatEquation(A, 100)
+
+    
+    C.pop(0)
+    C.pop()
+    
+    for k in SSP:
+        plt.plot(C,k)
+    
+    
+def Wavesoln():
+
+
+    C = GenerateChebyshevCollocationPoints(20)
+    C.sort()
+    
+    A = []
+    for i in C:
+        A.append(m.exp(-20.0*i**2))
+    A = np.array(A)
+    
+    V = []
+    for i in C:
+        V.append(5.0)
+        
+    V = np.array(V)
+    
+    SSP = PsuedoSpectralSolutionToWaveEquation(A, V, 500) 
+       
+    
+    C.pop(0)
+    C.pop()
+    
+    for i in range(0,30):
+        plt.plot(C,SSP[i])
+    
+        
+    
         
         
         
